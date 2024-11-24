983361407b072c1ee2cb63b5c8ee73b1f51f482a
gabriellyheber22 <gabriellyheber@gmail.com>
2024-11-24 19:24:45 -0300

    Base da Lista de Tarefas

---
+++ toDos.js
@@ -0,0 +1,4 @@
+let listElements = document.querySelector('#App ul');
+let inputElements = document.querySelector('#App input');
+let btnElements = document.querySelector('#App button');
+let toDos = ['Fazer café', 'Estudar JavaScript', 'Acessar comunidade da Rocketset']
