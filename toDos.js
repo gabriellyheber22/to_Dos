57d25f60a82956bb14bb4091d70ec4fe91acf196
gabriellyheber22 <gabriellyheber@gmail.com>
2024-11-24 20:06:56 -0300

    Criação da função de Adicionar

--- toDos.js
+++ toDos.js
@@ -1,4 +1,25 @@
-let listElements = document.querySelector('#App ul');
+var listElements = document.querySelector('#App ul');
 let inputElements = document.querySelector('#App input');
 let btnElements = document.querySelector('#App button');
 let toDos = ['Fazer café', 'Estudar JavaScript', 'Acessar comunidade da Rocketset']
+
+function renderTodos(){
+    listElements.innerText ='';
+    for(let todo of toDos){
+        let toDoElement = document.createElement('li');
+        let toDoText = document.createTextNode(todo);
+        toDoElement.appendChild(toDoText);
+        listElements.appendChild(toDoElement);
+
+    }
+}
+renderTodos();
+
+function AdicionaToDos(){
+    let toDoText = inputElements.value;
+    toDos.push(toDoText);
+    inputElements.value ='';
+    renderTodos();
+}
+
+btnElements.onclick = AdicionaToDos;
