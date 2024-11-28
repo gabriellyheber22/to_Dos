222cc43a36f6d78b43f0436b3510727e2015dc11
gabriellyheber22 <gabriellyheber@gmail.com>
2024-11-28 10:22:41 -0300

    Atualização da função de excluir

--- toDos.js
+++ toDos.js
@@ -1,67 +1,86 @@
 var listElements = document.getElementById('lista');
 let inputElements = document.getElementById('inputTarefa');
-let btnElements = document.querySelector('#App button');
+let btnElements = document.getElementById('btn');
+let btnExcluir = document.getElementById('btn-Excluir');
 var toDos = JSON.parse(localStorage.getItem('list_todo')) || []; //Inicialização padrão, converte os itens armazenados na localStorage para array ou se o local Storage estiver vazio, ele apresenta o array vazio
 let contadorId = 0;
 
-function renderTodos(){
-  
-    listElements.innerText ='';
-    for(let toDo of toDos){ //Visualizando os itens do array "toDo é o valor do array" e "toDos é o array"
+function renderTodos() {
+
+    listElements.innerText = '';
+    for (let toDo of toDos) { //Visualizando os itens do array "toDo é o valor do array" e "toDos é o array"
         let posicaoArray = toDos.indexOf(toDo);
         let toDoText = document.createTextNode(toDo);
-        
+
         let elements = document.createElement('div');
         elements.setAttribute('class', 'listaItem')
-       
-        
+
+
 
         let toDoElement = document.createElement('input');//Cria Input 
         toDoElement.setAttribute('type', 'checkbox'); //Atribui type:CheckBox
-        toDoElement.setAttribute('id', 'check'+posicaoArray );//Atribui Id ao input
-        toDoElement.setAttribute('name', 'check'+posicaoArray);
-        
-        
+        toDoElement.setAttribute('id', 'check' + posicaoArray);//Atribui Id ao input
+        toDoElement.setAttribute('name', 'check' + posicaoArray);
+
+
         let toDoLabel = document.createElement('label'); //Cria a Label
-        toDoLabel.setAttribute('for', 'check'+posicaoArray) //Informa o ID do Rotulo
+        toDoLabel.setAttribute('for', 'check' + posicaoArray) //Informa o ID do Rotulo
         toDoLabel.appendChild(toDoText);
 
-       
-        let quebraLinha= document.createElement('br')
-        
+
+        let quebraLinha = document.createElement('br')
+
         elements.appendChild(toDoElement);
         elements.appendChild(toDoLabel);
         elements.appendChild(quebraLinha);
         listElements.appendChild(elements);
-        
-      
-        
-       
+
+
+
+
+
+
+
         //Retorna o valor do indice (posição) onde o elemento referenciado esta localizado no array
-       // toDoElement.addEventListener('click', function () {
-          //  deleteTodo(posicaoArray); // Chama a função deleteTodo com a posição do item no array
+        // toDoElement.addEventListener('click', function () {
+        //  deleteTodo(posicaoArray); // Chama a função deleteTodo com a posição do item no array
         //});
     }
+
+
+
 }
 renderTodos();
 
-function AdicionaToDos(){
+function AdicionaToDos() {
     let toDoText = inputElements.value;
     toDos.push(toDoText);
-    inputElements.value ='';
+    inputElements.value = '';
     renderTodos();
     saveStorage();
 }
 
 btnElements.onclick = AdicionaToDos;
 
-function deleteTodo(pos){
-    toDos.splice(pos,1);
+function deleteTodo(pos) {
+    toDos.splice(pos, 1);
     renderTodos();
     saveStorage();
 
 }
 
-function saveStorage(){
-    localStorage.setItem('list_todo',JSON.stringify(toDos));
+btnExcluir.addEventListener('click', function () {
+    // Cria um novo array apenas com os itens não selecionados (não marcados)
+    toDos = toDos.filter((toDo, index) => {
+        let checkbox = document.getElementById('check' + index); // Encontra o checkbox pelo id
+        return !checkbox.checked; // Retorna true para os itens que NÃO estão marcados
+    });
+
+    // Re-renderiza a lista e salva no localStorage
+    renderTodos();
+    saveStorage();
+});
+
+function saveStorage() {
+    localStorage.setItem('list_todo', JSON.stringify(toDos));
 }
