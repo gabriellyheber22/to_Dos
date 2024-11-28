bf3c4a62e1fad6901fde52c2754efc3dd481356c
gabriellyheber22 <gabriellyheber@gmail.com>
2024-11-27 23:00:52 -0300

    Atualização de Estilização
    Mudança de lista de itens, pra checkbox

--- toDos.js
+++ toDos.js
@@ -1,43 +1,46 @@
-var listElements = document.querySelector('#App ul');
+var listElements = document.getElementById('lista');
 let inputElements = document.getElementById('inputTarefa');
 let btnElements = document.querySelector('#App button');
 var toDos = JSON.parse(localStorage.getItem('list_todo')) || []; //Inicialização padrão, converte os itens armazenados na localStorage para array ou se o local Storage estiver vazio, ele apresenta o array vazio
 let contadorId = 0;
 
 function renderTodos(){
-    //contadorId++;
-    ; 
+  
     listElements.innerText ='';
     for(let toDo of toDos){ //Visualizando os itens do array "toDo é o valor do array" e "toDos é o array"
         let posicaoArray = toDos.indexOf(toDo);
-        console.log(toDo);
         let toDoText = document.createTextNode(toDo);
+        
+        let elements = document.createElement('div');
+        elements.setAttribute('class', 'listaItem')
+       
         
 
         let toDoElement = document.createElement('input');//Cria Input 
         toDoElement.setAttribute('type', 'checkbox'); //Atribui type:CheckBox
         toDoElement.setAttribute('id', 'check'+posicaoArray );//Atribui Id ao input
         toDoElement.setAttribute('name', 'check'+posicaoArray);
-
+        
         
         let toDoLabel = document.createElement('label'); //Cria a Label
         toDoLabel.setAttribute('for', 'check'+posicaoArray) //Informa o ID do Rotulo
         toDoLabel.appendChild(toDoText);
 
        
+        let quebraLinha= document.createElement('br')
         
+        elements.appendChild(toDoElement);
+        elements.appendChild(toDoLabel);
+        elements.appendChild(quebraLinha);
+        listElements.appendChild(elements);
         
+      
         
-        
-        listElements.appendChild(toDoElement);
-        listElements.appendChild(toDoLabel);
-       
-       
        
         //Retorna o valor do indice (posição) onde o elemento referenciado esta localizado no array
-        toDoElement.addEventListener('click', function () {
-            deleteTodo(posicaoArray); // Chama a função deleteTodo com a posição do item no array
-        });
+       // toDoElement.addEventListener('click', function () {
+          //  deleteTodo(posicaoArray); // Chama a função deleteTodo com a posição do item no array
+        //});
     }
 }
 renderTodos();
